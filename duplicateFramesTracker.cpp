2eeba79ea97836d3dd0597a5d3bdd978a7645059
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2024-05-29 10:13:11 +0200

    Refactored duplicateFramesTracker to organize the code. Replaced substract with absdiff

--- duplicateFramesTracker.cpp
+++ duplicateFramesTracker.cpp
@@ -1,84 +1,87 @@
 #include "duplicateFramesTracker.hpp"
 #include <utility>
 
-// TODO: Potentially build SSIM method (Structural Similarity Index) to compare frames
+int	getLuminanceDiff(cv::Mat current, cv::Mat previous) {
 
-//bool DuplicateFramesTracker::findDuplicates(cv::Mat current, cv::Mat previous) {
-//    if (previous.empty())
-//        return false;
-//
-//    cv::Mat diff;
-//    cv::subtract(current, previous, diff);
-//    cv::cvtColor(diff, diff, cv::COLOR_BGR2GRAY);
-//
-//    int nonZero = cv::countNonZero(diff); // nonZero return: Number of non-zero pixels in the image
-//    int nonZeroThreshold = current.total() * 0.005; // 0.5% of the total pixels in the image
-//
-//    return nonZero < nonZeroThreshold;
-//}
+	cv::Mat ycrcbCurrent, ycrcbPrevious;
+	cv::cvtColor(current, ycrcbCurrent, cv::COLOR_BGR2YCrCb);
+	cv::cvtColor(previous, ycrcbPrevious, cv::COLOR_BGR2YCrCb);
+
+	// Split the YCrCb images into their channels. Luminance is 0
+	std::vector<cv::Mat> channelsCurrent, channelsPrevious;
+	cv::split(ycrcbCurrent, channelsCurrent);
+	cv::split(ycrcbPrevious, channelsPrevious);
+
+	cv::Mat luminanceDiff;
+	cv::absdiff(channelsCurrent[0], channelsPrevious[0], luminanceDiff);
+
+	return cv::countNonZero(luminanceDiff);
+}
 
 bool DuplicateFramesTracker::findDuplicates(cv::Mat current, cv::Mat previous) {
+
     if (previous.empty())
         return false;
 
+	int nonZeroThreshold = current.total() * 0.005; // 0.5% of the total pixels in the image
     cv::Mat diff;
-    cv::subtract(current, previous, diff);
+    cv::absdiff(current, previous, diff);
     cv::cvtColor(diff, diff, cv::COLOR_BGR2GRAY);
 
-    int nonZero = cv::countNonZero(diff); // nonZero return: Number of non-zero pixels in the image
-    int nonZeroThreshold = current.total() * 0.005; // 0.5% of the total pixels in the image
+    int nonZero = cv::countNonZero(diff);
 
     if (nonZero >= nonZeroThreshold) {
         return false;
     }
+    return (getLuminanceDiff(current, previous) < nonZeroThreshold);
+}
 
-    // YCrCb color space lets the program compare luminance
-    cv::Mat ycrcbCurrent, ycrcbPrevious;
-    cv::cvtColor(current, ycrcbCurrent, cv::COLOR_BGR2YCrCb);
-    cv::cvtColor(previous, ycrcbPrevious, cv::COLOR_BGR2YCrCb);
+bool	checkBlackFrame(cv::Scalar avgPixelIntensity) {
 
-    // Split the YCrCb images into their channels, as luminance is 0
-    std::vector<cv::Mat> channelsCurrent, channelsPrevious;
-    cv::split(ycrcbCurrent, channelsCurrent);
-    cv::split(ycrcbPrevious, channelsPrevious);
+	if (avgPixelIntensity[0] < 10 && avgPixelIntensity[1] < 10 && avgPixelIntensity[2] < 10)
+		return true;
+	return false;
+}
 
-    // Calculate the absolute difference between the Y channels (luminance) of the two frames
-    cv::Mat luminanceDiff;
-    cv::absdiff(channelsCurrent[0], channelsPrevious[0], luminanceDiff);
+bool checkSolidColorFrame(cv::Mat current, cv::Scalar avgPixelIntensity) {
 
-    int nonZeroLuminance = cv::countNonZero(luminanceDiff);
-    int nonZeroLuminanceThreshold = luminanceDiff.total() * 0.005; // 0.5% of the total pixels in the image
+	cv::Mat stdDev;
+	cv::meanStdDev(current, avgPixelIntensity, stdDev);
+	if (*stdDev.ptr<double>(0) < 1 && *stdDev.ptr<double>(1) < 1 && *stdDev.ptr<double>(2) < 1)
+		return true;
+	return false;
+}
 
-    return nonZeroLuminance < nonZeroLuminanceThreshold;
+bool checkTextFrame(cv::Mat current) {
+	cv::Mat gray, contrast;
+
+	cv::cvtColor(current, gray, cv::COLOR_BGR2GRAY);
+	cv::Laplacian(gray, contrast, CV_64F); // To catch high contrast edges (usually white text over black background qualifies)
+	double contrastLevel = cv::mean(contrast)[0];
+	if (contrastLevel > 10.0)
+		return true;
+	return false;
 }
 
 void DuplicateFramesTracker::recordDuplicateFrames(const cv::Mat& current, const cv::Mat& previous, const cv::VideoCapture& cap) {
-    const int uniqueFrameThreshold = 10;
 
     if (previous.empty())
         return;
 
-    // Check if the current frame is a black frame
-    cv::Scalar avgPixelIntensity = cv::mean(current);
-    if (avgPixelIntensity[0] < 10 && avgPixelIntensity[1] < 10 && avgPixelIntensity[2] < 10) {
+	cv::Scalar avgPixelIntensity = cv::mean(current);
+	const int uniqueFrameThreshold = 10;
+
+	if (checkBlackFrame(avgPixelIntensity)) {
 		m_start = -1;
 		return;
 	}
 
-	// Check if the current frame is a solid color frame
-	cv::Mat stdDev;
-	cv::meanStdDev(current, avgPixelIntensity, stdDev);
-	if (stdDev.at<double>(0) < 1 && stdDev.at<double>(1) < 1 && stdDev.at<double>(2) < 1) {
+	if (checkSolidColorFrame(current, avgPixelIntensity)) {
 		m_start = -1;
 		return;
 	}
 
-    // Check if the current frame is a text frame (high contrast)
-    cv::Mat gray, contrast;
-    cv::cvtColor(current, gray, cv::COLOR_BGR2GRAY);
-    cv::Laplacian(gray, contrast, CV_64F); // To catch high contrast edges (usually white text over black background qualifies)
-    double contrastLevel = cv::mean(contrast)[0];
-    if (contrastLevel > 10.0) {
+	if (checkTextFrame(current)) {
 		m_start = -1;
 		return;
 	}
