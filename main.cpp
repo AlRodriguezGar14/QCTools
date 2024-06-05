724742568a03d1bbb1c9877e88f37fead96259be
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2024-06-05 21:01:27 +0200

    Build: Split the video in four parts, then execute the checks sinultaneously in four threads to reduce the execution time. TODO: Merge the results. Pontential TODO: Build some kindof progression or loading bar

--- main.cpp
+++ main.cpp
@@ -1,86 +1,63 @@
-#include <iostream>
-#include <string>
 #include <opencv2/opencv.hpp>
+#include <future>
 #include "InactivePixelsTracker.hpp"
 #include "duplicateFramesTracker.hpp"
 
-void	printProgressBar(int curr, int total) {
-	float	progress = (float)curr / total;
-	int 	barWidth = 70;
-	int 	position = barWidth * progress;
+std::tuple<InactivePixelsTracker, DuplicateFramesTracker> processSegment(std::string videoPath, int startFrame, int endFrame, double fps) {
+	cv::VideoCapture cap(videoPath);
+	cap.set(cv::CAP_PROP_POS_FRAMES, startFrame);
 
-	std::cout << "[";
-	for (int i = 0; i != barWidth; i++) {
-		i <= position ? std::cout << "#" : std::cout << " ";
+	InactivePixelsTracker ipt(fps);
+	DuplicateFramesTracker dft(fps);
+
+	cv::Mat previous;
+	for (int i = startFrame; i < endFrame; ++i) {
+		cv::Mat frame;
+		bool success = cap.read(frame);
+
+		if (!success) {
+			std::cout << "Failed to read frame " << i << std::endl;
+			break;
+		}
+
+		dft.recordDuplicateFrames(frame, previous, cap);
+		ipt.recordInactivePixels(frame, cap);
+
+		previous = frame;
 	}
-	std::cout << "] " << curr << " / " << total << " | " << int(progress * 100) << "%";
-	if (curr != total)
-		std::cout << "\r";
-	else
-		std::cout << "\n";
-	std::cout.flush();
 
+	// ipt.printInactivePixels();
+	// dft.printDuplicateFrames();
+	return std::make_tuple(ipt, dft);
 }
 
 int main(int argc, char **argv) {
-
 	if (argc != 2) {
 		std::cout << "Usage: ./qctool <Video_Path>" << std::endl;
 		return 1;
 	}
 
-	cv::VideoCapture cap(argv[1]);
+	std::string videoPath = argv[1];
+	cv::VideoCapture cap(videoPath);
 	int totalFrames = cap.get(cv::CAP_PROP_FRAME_COUNT);
 	double fps = cap.get(cv::CAP_PROP_FPS);
 	std::cout << "fps: " << fps << std::endl;
 
+	std::future<std::tuple<InactivePixelsTracker, DuplicateFramesTracker>> fut1 = std::async(std::launch::async, processSegment, videoPath, 0, totalFrames/4, fps);
+    std::future<std::tuple<InactivePixelsTracker, DuplicateFramesTracker>> fut2 = std::async(std::launch::async, processSegment, videoPath, totalFrames/4, totalFrames/2, fps);
+    std::future<std::tuple<InactivePixelsTracker, DuplicateFramesTracker>> fut3 = std::async(std::launch::async, processSegment, videoPath, totalFrames/2, totalFrames*3/4, fps);
+    std::future<std::tuple<InactivePixelsTracker, DuplicateFramesTracker>> fut4 = std::async(std::launch::async, processSegment, videoPath, totalFrames*3/4, totalFrames, fps);
 
-	std::string window_name = argv[1];
-	cv::namedWindow(window_name, cv::WINDOW_NORMAL);
-	InactivePixelsTracker ipt(fps);
-	DuplicateFramesTracker dft(fps);
+    std::tuple<InactivePixelsTracker, DuplicateFramesTracker> res1 = fut1.get();
+    std::tuple<InactivePixelsTracker, DuplicateFramesTracker> res2 = fut2.get();
+    std::tuple<InactivePixelsTracker, DuplicateFramesTracker> res3 = fut3.get();
+    std::tuple<InactivePixelsTracker, DuplicateFramesTracker> res4 = fut4.get();
 
-//	cap.set(cv::CAP_PROP_POS_FRAMES, 89600);
 
-	cv::Mat previous;
-	int currFrame = 0;
-	while (true) {
+	std::get<1>(res1).printDuplicateFrames();
+	std::get<1>(res2).printDuplicateFrames();
+	std::get<1>(res3).printDuplicateFrames();
+	std::get<1>(res4).printDuplicateFrames();
 
-		cv::Mat frame;
-		bool bSuccess = cap.read(frame);
-		if (!bSuccess) {
-			std::cout << "End of the video" << std::endl;
-			break ;
-		}
-
-		// Find duplicate frames or freeze image
-		dft.recordDuplicateFrames(frame, previous, cap);
-		// Find inactive pixels, letterbox, and pillarbox
-		ipt.recordInactivePixels(frame, cap);
-
-		// Display frame, pending to check if display or not in the input
-//		cv::Mat gray;
-//		cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
-//		cv::equalizeHist(gray, gray);
-//		cv::threshold(gray, gray, 3, 255, cv::THRESH_BINARY);
-//		cv::imshow(window_name, gray);
-//		char key = cv::waitKey(1);
-//		if (key == 27)
-//			break ;
-//		else if (key == 112)
-//			while (true)
-//			{
-//				if (cv::waitKey(1) == 112)
-//					break ;
-//			}
-
-		printProgressBar(currFrame++, totalFrames - 1);
-		previous = frame;
-	}
-
-	ipt.printInactivePixels();
-	dft.printDuplicateFrames();
-	dft.printFreezeFrames();
-	cv::destroyAllWindows();
 	return 0;
 }
