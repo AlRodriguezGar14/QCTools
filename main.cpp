b1be3606756656336e9c31ff53f31dc0ba1a131e
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2024-06-06 01:12:14 +0200

    Major refactor and printing logic improvement
    Main.cpp: Refactor the logic of launching and retrieving the threads with an array and interations.
    
    dpfT: Improve the logic to not to add the frame 0 to 0 nor print empty timecode ranges.

--- main.cpp
+++ main.cpp
@@ -32,6 +32,7 @@ std::tuple<InactivePixelsTracker, DuplicateFramesTracker> processSegment(std::st
 }
 
 int main(int argc, char **argv) {
+
 	if (argc != 2) {
 		std::cout << "Usage: ./qctool <Video_Path>" << std::endl;
 		return 1;
@@ -42,22 +43,26 @@ int main(int argc, char **argv) {
 	int totalFrames = cap.get(cv::CAP_PROP_FRAME_COUNT);
 	double fps = cap.get(cv::CAP_PROP_FPS);
 	std::cout << "fps: " << fps << std::endl;
+	std::cout << "title: " << argv[1] << std::endl;
+	std::cout << "Processing..." << std::endl;
 
-	std::future<std::tuple<InactivePixelsTracker, DuplicateFramesTracker>> fut1 = std::async(std::launch::async, processSegment, videoPath, 0, totalFrames/4, fps);
-    std::future<std::tuple<InactivePixelsTracker, DuplicateFramesTracker>> fut2 = std::async(std::launch::async, processSegment, videoPath, totalFrames/4, totalFrames/2, fps);
-    std::future<std::tuple<InactivePixelsTracker, DuplicateFramesTracker>> fut3 = std::async(std::launch::async, processSegment, videoPath, totalFrames/2, totalFrames*3/4, fps);
-    std::future<std::tuple<InactivePixelsTracker, DuplicateFramesTracker>> fut4 = std::async(std::launch::async, processSegment, videoPath, totalFrames*3/4, totalFrames, fps);
-
-    std::tuple<InactivePixelsTracker, DuplicateFramesTracker> res1 = fut1.get();
-    std::tuple<InactivePixelsTracker, DuplicateFramesTracker> res2 = fut2.get();
-    std::tuple<InactivePixelsTracker, DuplicateFramesTracker> res3 = fut3.get();
-    std::tuple<InactivePixelsTracker, DuplicateFramesTracker> res4 = fut4.get();
+	int numberOfSegments = 4; // Update this to the number of threads to run
 
 
-	std::get<1>(res1).printDuplicateFrames();
-	std::get<1>(res2).printDuplicateFrames();
-	std::get<1>(res3).printDuplicateFrames();
-	std::get<1>(res4).printDuplicateFrames();
+
+	// What's happening in the background of the for loop:
+	// std::future<std::tuple<InactivePixelsTracker, DuplicateFramesTracker>> fut1 = std::async(std::launch::async, processSegment, videoPath, 0, totalFrames/4, fps);
+	std::vector<std::future<std::tuple<InactivePixelsTracker, DuplicateFramesTracker>>> futures;
+	for (int i = 0; i < numberOfSegments; ++i) {
+		int startFrame = i * totalFrames / numberOfSegments;
+		int endFrame = (i + 1) * totalFrames / numberOfSegments;
+		futures.push_back(std::async(std::launch::async, processSegment, videoPath, startFrame, endFrame, fps));
+	}
+	for (std::future<std::tuple<InactivePixelsTracker, DuplicateFramesTracker>> &fut : futures) {
+		std::tuple<InactivePixelsTracker, DuplicateFramesTracker> res = fut.get();
+		std::get<1>(res).printDuplicateFrames();
+		// TODO: Merge the results from the different segments with the corresponding classes
+	}
 
 	return 0;
 }
