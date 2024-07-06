eb0a42c9b2e966cfff8adfec376c33b00e278b69
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2024-07-07 00:54:38 +0200

    Build: Improved the video player so that it is closer to my ideal player idea

--- template.js
+++ template.js
@@ -1,6 +1,11 @@
 var video = document.getElementById('video');
+var playBtn = document.getElementById('play');
+var progress = document.querySelector('.progress');
+var progressBar = document.querySelector('.progress-bar');
+var playSpeed = document.getElementById('play-speed');
 var currentFrameElement = document.getElementById('currentFrame');
-var fps = {{fps}}
+var fps = {{m_fps}}
+// var fps = 23.976;
 
 video.addEventListener('timeupdate', function() {
     var currentFrame = Math.round(video.currentTime * fps);
@@ -30,3 +35,60 @@ function seek(frame) {
         navigator.clipboard.writeText(match[1]);
     }
 }
+
+
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+function updatePlayBtn() {
+    playBtn.innerHTML = video.paused ? "Play" : "Pause";
+}
+
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'k' || e.key === ' ') {
+        togglePlay();
+        video.playbackRate = 1;
+    }
+    if (e.key === 'l') {
+        if (video.paused)
+            togglePlay();
+        else
+            video.playbackRate += 0.5;
+    }
+    if (e.key === 'j') {
+        if (!video.paused)
+            video.playbackRate -= 0.25;
+    }
+    if (e.key === 'h') {
+        video.currentTime -= 3;
+    }
+    playSpeed.innerHTML = 'Speed: ' + video.playbackRate + 'x';
+});
+video.addEventListener('play', updatePlayBtn);
+video.addEventListener('pause', updatePlayBtn);
+
+
+video.addEventListener("loadedmetadata", function() {
+  var videoWidth = video.offsetWidth;
+  progress.style.width = videoWidth + 'px';
+});
+function handleProgress() {
+    const progressPercent = (video.currentTime / video.duration) * 100;
+    progressBar.style.width = `${progressPercent}%`;
+}
+function jump(e) {
+    const jumpTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = jumpTime;
+}
+video.addEventListener('timeupdate', handleProgress);
+progress.addEventListener('click', jump);
+let mousedown = false;
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mousemove', (e) => mousedown && jump(e));
+progress.addEventListener('mouseup', () => mousedown = false);
